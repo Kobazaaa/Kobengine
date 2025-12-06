e28079f3c6a853b42e3bcc66f1328315dcfd76be
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-12-06 01:10:47 +0100

    Add Debug logging
    for now it just forwards it through an event. But in the future we could write it to a file as well or do other stuff with the debug logs.

---
+++ engine/src/helper/Debug.cpp
@@ -0,0 +1,19 @@
+﻿// -- Kobengine Includes --
+#include "Debug.h"
+
+
+//--------------------------------------------------
+//    Logging
+//--------------------------------------------------
+void kobengine::Debug::Log(const std::string& msg)
+{
+	OnMessageLogged.Invoke(msg, LogSeverity::Normal);
+}
+void kobengine::Debug::LogWarning(const std::string& msg)
+{
+	OnMessageLogged.Invoke(msg, LogSeverity::Warning);
+}
+void kobengine::Debug::LogError(const std::string& msg)
+{
+	OnMessageLogged.Invoke(msg, LogSeverity::Error);
+}
---
+++ engine/src/helper/Debug.h
@@ -0,0 +1,44 @@
+﻿#ifndef KOBENGINE_DEBUG_H
+#define KOBENGINE_DEBUG_H
+
+// -- Kobengine Includes --
+#include "Event.h"
+
+// -- Standard Library --
+#include <string>
+
+namespace kobengine
+{
+    //? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //? ~~    Severity
+    //? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    enum class LogSeverity
+    {
+        Normal,
+        Warning,
+        Error
+    };
+
+    //? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //? ~~    Debug
+    //? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    class Debug
+    {
+    public:
+        //--------------------------------------------------
+		//    Constructor
+		//--------------------------------------------------
+        explicit Debug() = delete;
+
+		//--------------------------------------------------
+		//    Logging
+		//--------------------------------------------------
+        static void Log(const std::string& msg);
+        static void LogWarning(const std::string& msg);
+        static void LogError(const std::string& msg);
+
+        static inline Event<const std::string&, LogSeverity> OnMessageLogged;
+    };
+}
+
+#endif // KOBENGINE_DEBUG_H
