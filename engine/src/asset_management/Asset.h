48f79edacbc132176a92739ab5d20891624b7914
Kobe Dereyne <kobe.dereyne@outlook.com>
2026-03-15 14:12:14 +0100

    Implement Asset interface

---
+++ engine/src/asset_management/Asset.h
@@ -0,0 +1,56 @@
+﻿#ifndef KOBENGINE_ASSET_H
+#define KOBENGINE_ASSET_H
+
+// -- Standard Library --
+#include <string>
+
+namespace kobengine
+{
+	//? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//? ~~    Asset
+	//? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	class Asset
+	{
+	public:
+		//--------------------------------------------------
+		//    Constructor & Destructor
+		//--------------------------------------------------
+		explicit Asset(const std::string& id) : m_AssetID(id) {}
+		virtual ~Asset() = default;
+
+		Asset(const Asset& other) = default;
+		Asset(Asset&& other) noexcept = default;
+		Asset& operator=(const Asset& other) = default;
+		Asset& operator=(Asset&& other) noexcept = default;
+
+		//--------------------------------------------------
+		//    Functionality
+		//--------------------------------------------------
+		bool Load()
+		{
+			m_Loaded = DoLoad();
+			return m_Loaded;
+		}
+
+		void Unload()
+		{
+			DoUnload();
+		}
+
+		//--------------------------------------------------
+		//    Accessors & Mutators
+		//--------------------------------------------------
+		[[nodiscard]] const std::string& GetID() const { return m_AssetID; }
+		[[nodiscard]] bool IsLoaded() const { return m_Loaded; }
+
+	protected:
+		virtual bool DoLoad() = 0;
+		virtual bool DoUnload() = 0;
+
+	private:
+		std::string m_AssetID {};
+		bool m_Loaded { false };
+	};
+}
+
+#endif // KOBENGINE_ASSET_H
